adc507a611acacf528dd4fb548749f7b113a64e6
ap766 <anipatra733@gmail.com>
2024-08-30 09:21:50 +0530

    g

--- sdesheet/tree2/4.checkifbalanced/p2.py
+++ sdesheet/tree2/4.checkifbalanced/p2.py
@@ -1,54 +1,32 @@
-                                
-# Node class for the binary tree
-class Node:
-    def __init__(self, val):
-        self.data = val
-        self.left = None
-        self.right = None
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
-    # Function to check if a binary tree is balanced
-    def isBalanced(self, root):
-        return self.dfsHeight(root) != -1
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        def recur(root):
+            if root is None:
+                return 0
 
-    def dfsHeight(self, root):
-   
-        if not root:
-            return 0
+            l = recur(root.left)
+            if l == -1:
+                return -1
+            r = recur(root.right)
+            if r == -1:
+                return -1
 
-        left_height = self.dfsHeight(root.left)
+            if abs(l - r) > 1:
+                return -1 
+            
+            return max(l, r) + 1
+            
+        return recur(root) != -1
+    
 
-        if left_height == -1:
-            return -1
-
-        right_height = self.dfsHeight(root.right)
-
-        if right_height == -1:
-            return -1
-
-        if abs(left_height - right_height) > 1:
-            return -1
-
-        return max(left_height, right_height) + 1
-
-
-# Creating a sample binary tree
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-root.left.right.right = Node(6)
-root.left.right.right.right = Node(7)
-
-# Creating an instance of the Solution class
-solution = Solution()
-
-# Checking if the tree is balanced
-if solution.isBalanced(root):
-    print("The tree is balanced.")
-else:
-    print("The tree is not balanced.")                                   
-
-                                
-                            
+        
